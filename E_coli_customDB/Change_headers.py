1290f04571a6f0beb7e635d5f2ed8e2127b727cd
Max Cummins <max.l.cummins@gmail.com>
2018-04-24 16:53:02 +1000

    Fixed typo

--- E_coli_customDB/Change_headers.py
+++ E_coli_customDB/Change_headers.py
@@ -1,6 +1,6 @@
 #!/Users/maxcummins/miniconda2/envs/py36ipy/bin/python
 
-#used to edit headers of customDB_Feb_2018.fasta to make them compatable with ARIBAlord
+#used to edit headers of customDB_Feb_2018.fasta to make them compatible with ARIBAlord
 
 import re
 with open('../E_coli_customDB/EC_customDB.fasta', 'r+' ) as f:
